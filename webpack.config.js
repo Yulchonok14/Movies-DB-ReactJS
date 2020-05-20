cd3d4ff648747371888617788f8facd27864b2cb
Yulchonok14 <yulia.europa@mail.ru>
2020-05-20 17:21:45 +0300

    Change mode for NODE_ENV

--- webpack.config.js
+++ webpack.config.js
@@ -1,59 +1,58 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
+const isProd = process.env.NODE_ENV.trim() === 'production';
 
-module.exports = ( env, argv ) => {
-    const isProd = argv.mode === 'production';
-    return ({
-        entry: './src/index.js',
-        output: {
-            path: path.resolve(__dirname, 'dist'),
-            filename: 'bundle.js',
-            publicPath: ''
-        },
-        devtool: isProd ? 'none' : 'cheap-module-eval-source-map',
-        module: {
-            rules: [
-                {
-                    test: /\.js$/,
-                    loader: 'babel-loader',
-                    exclude: /node_modules/
-                },
-                {
-                    test: /\.css$/,
-                    exclude: /node_modules/,
-                    use: [
-                        {loader: 'style-loader'},
-                        {
-                            loader: 'css-loader',
-                            options: {
-                                importLoader: 1,
-                                modules: {
-                                    localIdentName: '[name]__[local]__[hash:base64:5]'
-                                }
-                            }
-                        },
-                        {
-                            loader: 'postcss-loader',
-                            options: {
-                                ident: 'postcss',
-                                plugins: [require('autoprefixer')()]
+module.exports = {
+    mode: process.env.NODE_ENV.trim(),
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+        publicPath: ''
+    },
+    devtool: isProd ? 'none' : 'cheap-module-eval-source-map',
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.css$/,
+                exclude: /node_modules/,
+                use: [
+                    {loader: 'style-loader'},
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            importLoader: 1,
+                            modules: {
+                                localIdentName: '[name]__[local]__[hash:base64:5]'
                             }
                         }
-                    ]
-                },
-                {
-                    test: /\.(png|jpe?g|gif)$/,
-                    loader: 'url-loader?limit=8000&name=images/[name].[ext]'
-                }
-            ]
-        },
-        plugins: [
-            new HtmlWebpackPlugin({
-                template: __dirname + '/src/index.html',
-                filename: 'index.html',
-                inject: 'body'
-            })
+                    },
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            ident: 'postcss',
+                            plugins: [require('autoprefixer')()]
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/,
+                loader: 'url-loader?limit=8000&name=images/[name].[ext]'
+            }
         ]
-    });
-}
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: __dirname + '/src/index.html',
+            filename: 'index.html',
+            inject: 'body'
+        })
+    ]
+};
